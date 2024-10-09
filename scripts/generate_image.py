9ed4589b0c5f985bdcadf326c5c20dbf1700c57e
Chanceium <71226606+Chanceium@users.noreply.github.com>
2024-10-09 17:01:26 -0400

    Update generate_image.py

--- scripts/generate_image.py
+++ scripts/generate_image.py
@@ -1,12 +1,13 @@
+import os
+import argparse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 import requests
 import urllib.request
-from selenium.webdriver.chrome.options import Options
-import os
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 def is_server_reachable(url):
     try:
@@ -17,12 +18,12 @@ def is_server_reachable(url):
         print(f"Error reaching the server: {e}")
         return False
 
-# Define the API URL
+# Define the URLs
 web_ui_url = "http://172.30.1.6:9091/"
+api_url = "http://172.30.1.6:9091/api/v1/images/"
 
-# Check if the web UI server is reachable
 if not is_server_reachable(web_ui_url):
-    print("Server is not reachable. Exiting.")
+    print("Web UI server is not reachable. Exiting.")
     exit(1)
 
 recipe_id = os.getenv('RECIPE_ID')
@@ -30,16 +31,15 @@ food_prompt = os.getenv('PROMPT')
 
 print("Configuring WebDriver options")
 # Configure WebDriver options for headless mode
-chrome_options = Options()
+chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
 
 print("Initializing WebDriver in headless mode")
 # Initialize WebDriver
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
 print("Opening the web UI")
 # Open the web UI
@@ -53,7 +53,6 @@ try:
     use_default_settings_button.click()
     
     print("Waiting for the prompt input to be present")
-    # Wait for the input element for the prompt to be present
     prompt_input = WebDriverWait(driver, 20).until(
         EC.presence_of_element_located((By.ID, "prompt"))
     )
@@ -61,7 +60,6 @@ try:
     prompt_input.send_keys(food_prompt)
 
     print("Waiting for the generate button to be clickable")
-    # Wait for the button to be clickable and click it
     generate_button = WebDriverWait(driver, 20).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, ".chakra-button.css-cos6y7"))
     )
@@ -74,15 +72,11 @@ finally:
     print("Closing the WebDriver")
     driver.quit()
 
-print("Fetching the latest image from the API")
-# Define the API URL to fetch the latest image
-api_url = "http://172.30.1.6:9091/api/v1/images/"
-
-# Check if the API server is reachable
 if not is_server_reachable(api_url):
     print("API server is not reachable. Exiting.")
     exit(1)
 
+print("Fetching the latest image from the API")
 # Send a GET request to the API to fetch image details
 response = requests.get(api_url, params={"limit": 1, "offset": 0})
 
@@ -100,3 +94,4 @@ if response.status_code == 200:
 else:
     print("Failed to fetch image details.")
     print("Status code:", response.status_code)
+    print("Response:", response.text)
